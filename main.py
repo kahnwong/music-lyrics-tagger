c02789ce562c4bb1fe480f9d1cd3c61d6be75e85
Karn Wong <karn@karnwong.me>
2023-02-24 00:01:19 +0700

    get metadata

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import glob
+import logging as log
+from typing import List
 
-# import mutagen
+import mutagen
+
 # import azapi
+log.basicConfig(format="%(asctime)s - [%(levelname)s] %(message)s", level=log.DEBUG)
 
 
-def _get_files(path: str):
+def _get_files(path: str) -> List[str]:
     extensions = ["mp3", "flac", "m4a"]
     search_patterns = [f"{path}/**/*.{ext}" for ext in extensions]
 
@@ -15,7 +19,27 @@ def _get_files(path: str):
     return files
 
 
+def _get_metadata(file: str):
+    metadata = mutagen.File(file).tags
+    # print(metadata.keys())
+    extension = file.split(".")[-1]
+
+    if extension == "m4a":
+        return {
+            "extension": extension,
+            "artist": metadata["©ART"],
+            "title": metadata["©nam"],
+        }
+
+
 if __name__ == "__main__":
-    path = r"D:\Music\# Pop\Abalone Dots"
+    path = r"D:\Music\# Rock\Amanda Somerville"
+    # path = r"D:\# tagginig\Music\z-staging\Avril Lavigne"
     files = _get_files(path)
-    print(files)
+
+    for i in files:
+        log.info(f"Processing: {i}")
+
+        print(_get_metadata(i))
+
+        break
